ba1c5871a19c194dadf514a82cf5b170628c5ce8
M4322uni <piscitello.2130445@studenti.uniroma1.it>
2026-02-08 18:40:15 +0100

    added hashing, backtracking to be tested

--- include/.base/hashing.hpp
+++ include/.base/hashing.hpp
@@ -1,14 +1,26 @@
 #pragma once
 
+#include <cstddef>
 #include <functional>
+#include <vector>
 
-// TODO: documentation
-template<>
-struct std::hash<std::pair<size_t, size_t>> {
-     size_t operator()(const std::pair<size_t, size_t>& p) const noexcept {
+// TODO: documentation (?)
+template<typename T, typename R>
+struct std::hash<std::pair<T, R>> {
+     size_t operator()(const std::pair<T, R>& p) const noexcept {
           size_t ret = 7;          
-          ret = (ret + std::hash<size_t>()(p.first)) * 31;
-          ret = (ret + std::hash<size_t>()(p.second)) * 31;
+          ret = (ret + std::hash<T>()(p.first)) * 31;
+          ret = (ret + std::hash<R>()(p.second)) * 31;
+          return ret;
+    }
+};
+
+template<typename T>
+struct std::hash<std::vector<T>> {
+     size_t operator()(const std::vector<T>& p) const noexcept {
+          size_t ret = 7;     
+          for (const auto &e : p)     
+               ret = (ret + std::hash<T>()(e)) * 31;
           return ret;
     }
 };
--- include/utils/backtracking.hpp
+++ include/utils/backtracking.hpp
@@ -1,16 +1,64 @@
 #pragma once
+#include <cstddef>
 #include <functional>
+#include <limits>
+#include <memory>
 #include <unordered_set>
 #include <utility>
 #include <vector>
 
-enum class arg_strat {
-    MIN, MAX
-};
+// TODO: documentation
+namespace isw {
+    
+    enum class arg_strat {
+        MIN, MAX
+    };
 
-template<typename param_t, typename res_t>
-std::unordered_set<std::vector<param_t>> argmin_max(std::vector<std::pair<param_t, param_t>> ranges, 
-                                                    std::function<res_t(std::vector<param_t>)> f);
+    template<typename param_t>
+    using bucket = std::shared_ptr<std::unordered_set<std::vector<param_t>>>;
 
-template<typename param_t, typename res_t>
-void f();
+    template<typename param_t, typename res_t>
+    using couple = std::pair<bucket<param_t>, res_t>;
+
+    template<typename param_t, typename res_t>
+    couple<param_t, res_t> backtrack(std::vector<std::pair<param_t, param_t>> &ranges, std::shared_ptr<std::vector<param_t>> ancilla, arg_strat strategy, 
+                                                        couple<param_t, res_t> info, std::function<res_t(std::shared_ptr<std::vector<param_t>>)> f, std::size_t i = 0) {
+        if (i == ancilla->size()) {
+            res_t res = f(ancilla);
+            if (strategy == arg_strat::MAX ? res > info.second : res < info.second) {
+                info.first->clear();
+                info.first->insert(*ancilla);
+                info.second = res;
+            }
+            else if (res == info.second) {
+                info.first->insert(*ancilla);
+            }
+            return info;
+        }
+        param_t val;
+        couple<param_t, res_t> temp = info;
+        for (val = ranges[i].first; val <= ranges[i].second; val++) {
+            ancilla[i] = val;
+            temp = backtrack(ranges, ancilla, strategy, temp, f, i + 1);
+        }
+        return temp;
+    }
+
+    // to use only for discrete types
+    template<typename param_t, typename res_t>
+    bucket<param_t> arg_min_max(std::vector<std::pair<param_t, param_t>> &ranges, 
+                                std::function<res_t(std::shared_ptr<std::vector<param_t>>)> f, arg_strat strategy) {
+        couple<param_t, res_t> info = {std::make_shared<std::unordered_set<std::vector<param_t>>>(), 
+                    (strategy == arg_strat::MAX) ? std::numeric_limits<res_t>::lowest() : std::numeric_limits<res_t>::max()};
+        auto ancilla = std::make_shared<std::vector<param_t>>(ranges.size());
+        backtrack(ranges, ancilla, strategy, info, f);
+        return info.first;
+    }
+
+    template<typename param_t, typename res_t>
+    bucket<param_t> arg_min_max(std::vector<std::pair<param_t, param_t>> &&ranges, 
+                                                        std::function<res_t(std::shared_ptr<std::vector<param_t>>)> f, arg_strat strategy) {
+        auto rang = std::move(ranges);
+        return arg_min_max(rang, f, strategy);
+    }
+}
