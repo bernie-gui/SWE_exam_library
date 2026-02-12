96bcf5c8e6504bfc93159a7442f21ce3cf09de9a
M4322uni <piscitello.2130445@studenti.uniroma1.it>
2026-02-12 13:42:07 +0100

    added error for rate measurer

--- src/utils/rate.cpp
+++ src/utils/rate.cpp
@@ -24,12 +24,15 @@
  * Description:
  *	This file implements the rate_meas_t class methods for rate measurements in the simulation system.
  */
+#include <stdexcept>
 #include "utils/rate.hpp"
 using namespace isw::utils;
 
 rate_meas_t::rate_meas_t(): _rate(0), _last_time(0) {}
 
 void rate_meas_t::update(double amount, double time) {
+    if (!time) 
+        throw std::runtime_error("Math error in rate measurement: update method called at time zero");
     _rate = _rate * ( _last_time / time ) + amount / time;
     _last_time = time;
 }
