e6f243a1384ada63ec81cd6357efad51c2b37605
uniquadev <58218460+uniquadev@users.noreply.github.com>
2026-02-15 13:35:26 +0100

    updated amalgamate

--- amalgamate.hpp
+++ amalgamate.hpp
@@ -31,6 +31,49 @@
 
 #include <cstdint>
 #include <string>
+
+/*** Start of inlined file: hashing.hpp ***/
+#pragma once
+
+#include <cstddef>
+#include <functional>
+#include <vector>
+#include <tuple>
+
+// TODO: documentation (?)
+template<typename T, typename R>
+struct std::hash<std::pair<T, R>> {
+     size_t operator()(const std::pair<T, R>& p) const noexcept {
+          size_t ret = 7;
+          ret = (ret + std::hash<T>()(p.first)) * 31;
+          ret = (ret + std::hash<R>()(p.second)) * 31;
+          return ret;
+    }
+};
+
+template<typename T>
+struct std::hash<std::vector<T>> {
+     size_t operator()(const std::vector<T>& p) const noexcept {
+          size_t ret = 7;
+          for (const auto &e : p)
+               ret = (ret + std::hash<T>()(e)) * 31;
+          return ret;
+    }
+};
+
+template<typename T, typename R, typename Z>
+struct std::hash<std::tuple<T, R, Z>> {
+     size_t operator()(const std::tuple<T, R, Z>& t) const noexcept {
+          size_t ret = 7;
+          ret = (ret + std::hash<T>()(std::get<0>(t))) * 31;
+          ret = (ret + std::hash<R>()(std::get<1>(t))) * 31;
+          ret = (ret + std::hash<Z>()(std::get<2>(t))) * 31;
+          return ret;
+    }
+};
+/*** End of inlined file: hashing.hpp ***/
+
+
 /*
  * TYPES DEFINITIONs
  */
@@ -77,7 +120,7 @@ namespace isw::network
         double timestamp;               /**< @brief Time when the message was sent. */
         size_t sender;                  /**< @brief ID of the sending process. */
         size_t sender_rel;				/**< @brief Relative ID of the sending process. */
-        world_key_t world_key;        	/**< @brief Key identifying the world context of the message. */
+        world_key_t world_key;        	/**< @brief World key of the sending process. */
     };
 
     /** @brief Type alias for a message channel, implemented as a queue of shared pointers to messages. */
@@ -449,6 +492,9 @@ namespace isw
          * @return Shared pointer to this system.
          */
         std::shared_ptr< system_t > add_network( std::shared_ptr< network_t > net );
+        // TODO: add documentation
+        std::shared_ptr< system_t > add_pid_network( double obj_occupancy = 1, double th_time = 0.0 ,
+            double error_threshold = 0);
         /**
          * @brief Registers a process to a specific world.
          * @param[in] p Shared pointer to the process.
@@ -912,6 +958,7 @@ namespace isw
 
 /*** Start of inlined file: logger.hpp ***/
 #pragma once
+
 #include <filesystem>
 #include <fstream>
 #include <memory>
@@ -1094,36 +1141,11 @@ namespace isw
 
 
 /*** Start of inlined file: optimizer.hpp ***/
-/*
- * File: optimizer.hpp
- * Copyright (c) 2025 bernie_gui, uniquadev, SepeFr.
- *
- * This file is part of SWE_exam_library
- *
- * SWE_exam_library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SWE_exam_library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- * University: Sapienza University of Rome
- * Instructor: Enrico Tronci
- * Academic Year: 2025-2026
- *
- * Description:
- *	This header file defines the optimizer_t class and optimizer_strategy enum for optimization algorithms using Monte
- * Carlo methods.
- */
 #pragma once
+#include <algorithm>
 #include <cassert>
 #include <memory>
+#include <stdexcept>
 #include <vector>
 
 namespace isw
@@ -1139,6 +1161,7 @@ namespace isw
      * @brief Abstract base class for optimization algorithms using Monte Carlo methods.
      * @details Provides framework for running optimization by sampling parameters and evaluating objective functions.
      */
+    template <typename param_t = double>
     class optimizer_t
     {
     public:
@@ -1146,7 +1169,7 @@ namespace isw
          * @brief Constructor.
          * @param[in] montecarlo Shared pointer to the Monte Carlo simulator.
          */
-        optimizer_t( std::shared_ptr< global_t > global );
+        optimizer_t( std::shared_ptr< global_t > global ) : _global( global ) {};
 
         /*
          * This must run the simulation and make the calculations
@@ -1157,7 +1180,7 @@ namespace isw
          * @details This method must run the simulation and compute the objective value, storing it in global
          * optimizer_result.
          */
-        virtual double obj_fun( std::vector< double > &arguments ) = 0;
+        virtual double obj_fun( std::vector< param_t > &arguments ) = 0;
 
         /**
          * @brief Gets the global state, cast to type T.
@@ -1180,8 +1203,51 @@ namespace isw
          * @throws std::runtime_error If strategy is not implemented.
          * @details Samples random parameters within bounds, evaluates objective function, and tracks the best solution.
          */
-        void optimize( optimizer_strategy strategy, std::vector< double > min_solution,
-                       std::vector< double > max_solution );
+        void optimize( optimizer_strategy strategy, std::vector< param_t > min_solution, std::vector< param_t > max_solution ) {
+            assert( min_solution.size() == max_solution.size() );
+            size_t n_params = min_solution.size();
+            std::vector< param_t > arguments( n_params );
+            auto random = _global->get_random();
+            // sol parametro (argmuents)
+            // obj è lo scalare (risltato di obj_fun che lui scrive in global optimizer result)
+
+            double best_res_so_far = strategy == optimizer_strategy::MAXIMIZE ? std::numeric_limits< double >::lowest()
+                                                                            : std::numeric_limits< double >::max();
+            std::vector< param_t > best_param_so_far( n_params );
+
+            for ( size_t i = 0; i < _global->optimizer_budget(); i++ )
+            {
+                for ( size_t i = 0; i < n_params; i++ )
+                {
+                    arguments[i] = random->uniform_range( min_solution[i], max_solution[i] );
+                }
+                double obj_resul = obj_fun( arguments );
+                switch ( strategy )
+                {
+                    case optimizer_strategy::MINIMIZE:
+                        if ( obj_resul < best_res_so_far )
+                        {
+                            best_param_so_far = arguments;
+                            best_res_so_far = obj_resul;
+                        };
+                        break;
+                    case optimizer_strategy::MAXIMIZE:
+                        if ( obj_resul > best_res_so_far )
+                        {
+                            best_param_so_far = arguments;
+                            best_res_so_far = obj_resul;
+                        };
+                        break;
+                    default:
+                        throw std::runtime_error( "not implemented" );
+                }
+            }
+            // Store best result found so far
+            std::vector<double> temp(best_param_so_far.size());
+            std::copy(best_param_so_far.begin(), best_param_so_far.end(), temp.begin());
+            _global->set_optimizer_result( best_res_so_far );
+            _global->set_optimizer_optimal_parameters( temp );
+        }
 
         /**
          * @brief Runs the optimization process for single-dimensional parameter.
@@ -1190,7 +1256,9 @@ namespace isw
          * @param[in] max_solution Maximum bound.
          * @details Delegates to the vector version with single-element vectors.
          */
-        void optimize( optimizer_strategy strategy, double min_solution, double max_solution );
+        void optimize( optimizer_strategy strategy, param_t min_solution, param_t max_solution ) {
+            optimize( strategy, std::vector< param_t >( 1, min_solution ), std::vector< param_t >( 1, max_solution ) );
+        }
 
     private:
         /** @brief The Monte Carlo simulator instance. */
@@ -1209,7 +1277,6 @@ namespace isw
 #include <cassert>
 #include <filesystem>
 #include <fstream>
-#include <memory>
 
 namespace isw
 {
@@ -1224,10 +1291,9 @@ namespace isw
         /**
          * @brief Constructor.
          * @param[in] path Path to the input file.
-         * @param[in] global Shared pointer to global state.
          * @throws std::runtime_error If file cannot be opened.
          */
-        input_parser_t( std::shared_ptr< global_t > global,  const std::filesystem::path &path = "parameters.txt" );
+        input_parser_t( const std::filesystem::path &path = "parameters.txt" );
         ~input_parser_t();
 
         // // forward extraction to the underlying stream
@@ -1249,18 +1315,6 @@ namespace isw
          */
         std::ifstream &get_stream();
         /**
-         * @brief Gets the global state, cast to type T.
-         * @tparam T Type to cast to, defaults to global_t.
-         * @return Shared pointer to casted global.
-         */
-        template< typename T = global_t >
-        std::shared_ptr< T > get_global()
-        {
-            auto casted = std::dynamic_pointer_cast< T >( _global );
-            assert( casted );
-            return casted;
-        }
-        /**
          * @brief Resets the stream to the beginning.
          * @details Clears error flags and seeks to position 0.
          */
@@ -1269,21 +1323,8 @@ namespace isw
     private:
         /** @brief Input file stream. */
         std::ifstream _stream;
-        /** @brief Global state. */
-        std::shared_ptr< global_t > _global;
     };
-} // namespace isw
-
-//    std::string line;
-// while(std::getline(file, line))
-
-// ifstream in;
-// in >> whatever;
-/*
- *jjjkk
- *
- */
-
+}
 /*** End of inlined file: input_parser.hpp ***/
 
 
@@ -1293,8 +1334,6 @@ namespace isw
 
 #include <filesystem>
 #include <fstream>
-#include <memory>
-#include <sstream>
 
 namespace isw
 {
@@ -1355,6 +1394,7 @@ namespace isw
 
 /*** Start of inlined file: network.hpp ***/
 #pragma once
+
 #include <vector>
 
 namespace isw
@@ -1402,7 +1442,7 @@ namespace isw
         /** @brief Current index in the scanner list. */
         size_t _current; // scanned_idx
     };
-} // namespace isw
+}
 
 /*** End of inlined file: network.hpp ***/
 
